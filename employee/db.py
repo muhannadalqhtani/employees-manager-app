1fedb253f686d89ce53948781bfbe16bbd234f47
Mohammedalzzagi <153850225+Mohammedalzzagi@users.noreply.github.com>
2023-12-15 17:42:58 +0300

    Update db.py

--- employee/db.py
+++ employee/db.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+# قاعدة بيانات
 class Database:
     def __init__(self, db):
         self.con = sqlite3.connect(db)
@@ -17,24 +17,24 @@ class Database:
                 address TEXT
             )
         """
-
+         # اضف لي قاعدة بيانات
         self.cur.execute(sql)
         self.con.commit()
-
+      # ادخال  الى قاعدة بيانات
     def insert(self, name, age, job, email, gender, mobile, address):
         self.cur.execute("INSERT INTO employees VALUES (NULL, ?, ?, ?, ?, ?, ?, ?)",
                          (name, age, job, email, gender, mobile, address))
         self.con.commit()
-
+        # يحدد موظفين بعدها يرتب لك
     def fetch(self):
         self.cur.execute("SELECT * FROM employees")
         rows = self.cur.fetchall()
         return rows
-
+      # يحذف موظف من قاعدة بيانات
     def remove(self, id):
         self.cur.execute("DELETE FROM employees WHERE id=?", (id,))
         self.con.commit()
-
+      # تحديث قاعدة الموظفين في قاعدة بيانات
     def update(self, id, name, age, job, email, gender, mobile, address):
         self.cur.execute("UPDATE employees SET name=?, age=?, job=?, email=?, gender=?, mobile=?, address=? WHERE id=?",
                          (name, age, job, email, gender, mobile, address, id))
